2bf1d3ff1ebe52f78b9abdd10c7774986abbb189
Roland Weber <rolandweber@users.noreply.github.com>
2015-11-10 16:17:31 +0100

    test cases for the ArgsInterpreter

--- src/test/java/pityoulish/cmdline/ArgsInterpreterTest.java
+++ src/test/java/pityoulish/cmdline/ArgsInterpreterTest.java
@@ -11,19 +11,338 @@ import static org.junit.Assert.*;
 
 public class ArgsInterpreterTest
 {
-  @Test public void dummy()
+  public static class MockBackendHandler implements BackendHandler
   {
-    System.out.println("@@@ dummy test executed");
+    public int      numArgs;
+    public String[] receivedArgs;
+
+    /**
+     * Creates a new mock backend handler.
+     * If the last argument is "error", the handler throws an exception.
+     */
+    public MockBackendHandler(int argc)
+    {
+      numArgs = argc;
+    }
+
+    public void describeUsage(Appendable app, String eol, String cmd)
+      throws java.io.IOException
+    {
+      app.append("Backend:");
+      app.append(String.valueOf(numArgs));
+      app.append(' ');
+      app.append(cmd);
+      app.append(eol);
+    }
+
+    public int getArgCount()
+    {
+      return numArgs;
+    }
+
+    public void setBackend(String... args)
+      throws Exception
+    {
+      receivedArgs = args;
+      assertEquals("wrong number of backend arguments", numArgs, args.length);
+
+      if ((args.length > 0) && "error".equalsIgnoreCase(args[args.length-1]))
+         throw new Exception("MockBackendProblem");
+    }
+
+  } // class MockBackendHandler
+
+
+  public static class MockCommandHandler implements CommandHandler
+  {
+    public String   commandName;
+    public int      numArgs;
+    public String[] receivedArgs;
+
+    /**
+     * Creates a new mock command handler.
+     * The last argument determines the status on handling the command.
+     * Pass a non-integer string argument to trigger an exception.
+     * Commands without arguments always succeed with status 0.
+     *
+     * @param cmd       the name of the command to handle
+     * @param argc      the exact number of arguments to handle.
+     */
+    public MockCommandHandler(String cmd, int argc)
+    {
+      commandName = cmd;
+      numArgs = argc;
+    }
+
+    public void describeUsage(Appendable app, String eol)
+      throws java.io.IOException
+    {
+      app.append("Command:");
+      app.append(commandName);
+      app.append('#');
+      app.append(String.valueOf(numArgs));
+      app.append(eol);
+    }
+
+    public boolean supports(String name, String... args)
+    {
+      return commandName.equalsIgnoreCase(name) && (numArgs == args.length);
+    }
+
+
+    public int handle(String name, String... args)
+      throws Exception
+    {
+      receivedArgs = args;
+      assertTrue("wrong command", commandName.equalsIgnoreCase(name));
+      assertEquals("wrong number of arguments", numArgs, args.length);
+
+      int status = 0;
+      if (args.length > 0)
+         status = Integer.parseInt(args[args.length-1]);
+      return status;
+    }
+
+  } // class MockCommandHandler
+
+
+  /**
+   * Creates an interpreter with a default set of handlers.
+   * <ul>
+   * <li>Backend handler with two arguments</li>
+   * <li>Command handler for "none" without arguments</li>
+   * <li>Command handler for "one" with one argument</li>
+   * <li>Command handler for "two" with two arguments</li>
+   * <li>Command handler for "one" with two arguments</li>
+   * </ul>
+   *
+   * @return the argument interpreter
+   */
+  public static ArgsInterpreter createTestSubject()
+  {
+    MockBackendHandler mbh = new MockBackendHandler(2);
+    MockCommandHandler ch0 = new MockCommandHandler("none", 0);
+    MockCommandHandler ch1 = new MockCommandHandler("one", 1);
+    MockCommandHandler ch2 = new MockCommandHandler("two", 2);
+    MockCommandHandler ch12 = new MockCommandHandler("one", 2);
+
+    return new ArgsInterpreter(mbh, ch0, ch1, ch2, ch12);
   }
 
 
-  @Ignore @Test public void failure()
+  /**
+   * Asserts that the backend handler has been called with the expected args.
+   *
+   * @param args   the arguments for the backend, and possibly more
+   * @param argc   the number of arguments expected by the backend.
+   *            This number of arguments from the start of <code>args</code>
+   *            will be verified.
+   * @param mbh    the backend handler that should have been called
+   */
+  protected static void assertBackendArgs(String[] args, int argc,
+                                          MockBackendHandler mbh)
   {
-    fail("@@@ dummy test failure");
+    assertNotNull("backend handler not called", mbh.receivedArgs);
+    assertEquals("wrong number of backend args",
+                 argc, mbh.receivedArgs.length);
+
+    for (int i=0; i<argc; i++)
+     {
+       assertEquals("wrong backend arg #"+i, args[i], mbh.receivedArgs[i]);
+     }
   }
 
-  @Ignore @Test public void error()
+
+  /**
+   * Asserts that the command handler has been called with the expected args.
+   *
+   * @param args   the arguments for the command, and possibly more
+   * @param argc   the number of arguments expected by the command.
+   *            This number of arguments at the end of <code>args</code>
+   *            will be verified.
+   * @param mch    the command handler that should have been called
+   */
+  protected static void assertCommandArgs(String[] args, int argc,
+                                          MockCommandHandler mch)
   {
-    throw new Error("@@@ dummy test error");
+    assertNotNull("expected command handler not called", mch.receivedArgs);
+    assertEquals("wrong number of command args",
+                 argc, mch.receivedArgs.length);
+
+    for (int i=0; i<argc; i++)
+     {
+       assertEquals("wrong command arg #"+i,
+                    args[i + args.length - argc], mch.receivedArgs[i]);
+     }
   }
+
+
+  @Test public void getUsage()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+
+    String usage = ai.getUsage();
+
+    System.out.println(usage);
+    assertNotNull("no usage description", usage);
+    assertTrue("missing usage for backend args",
+               usage.indexOf("Backend:2") >= 0);
+    assertTrue("missing usage for command none, no arg",
+               usage.indexOf("Command:none#0") >= 0);
+    assertTrue("missing usage for command one, one arg",
+               usage.indexOf("Command:one#1") >= 0);
+    assertTrue("missing usage for command two, two args",
+               usage.indexOf("Command:two#2") >= 0);
+    assertTrue("missing usage for command one, two args",
+               usage.indexOf("Command:one#2") >= 0);
+  }
+
+
+  @Test public void handleNoArgs()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ };
+
+    int status = ai.handle(args);
+
+    assertFalse("unexpected success status", (0 == status));
+  }
+
+
+  @Test public void handleNoCommand()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port" };
+
+    int status = ai.handle(args);
+
+    assertFalse("unexpected success status", (0 == status));
+  }
+
+
+  @Test public void handleUnknownCommand()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "neverheardof" };
+
+    int status = ai.handle(args);
+
+    assertFalse("unexpected success status", (0 == status));
+  }
+
+
+  @Test public void handleTooFewArguments()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "two", "1" };
+
+    int status = ai.handle(args);
+
+    assertFalse("unexpected success status", (0 == status));
+  }
+
+
+  @Test public void handleTooManyArguments()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "two", "1", "2", "3" };
+
+    int status = ai.handle(args);
+
+    assertFalse("unexpected success status", (0 == status));
+  }
+
+
+  @Test public void handleNone_BackendProblem()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "error", "none" };
+
+    try {
+      int status = ai.handle(args);
+      fail("exception not thrown, status="+status);
+    } catch (Exception expected) {
+      assertEquals("wrong exception message",
+                   "MockBackendProblem", expected.getMessage());
+    }
+  }
+
+
+  @Test public void handleNone_OK()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "none" };
+
+    int status = ai.handle(args);
+
+    assertEquals("unexpected status", 0, status);
+    assertBackendArgs(args, 2, (MockBackendHandler) ai.backendHandler);
+    assertCommandArgs(args, 0, (MockCommandHandler) ai.cmdHandlers.get(0));
+  }
+
+
+  @Test public void handleOne_OK()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "one", "111" };
+
+    int status = ai.handle(args);
+
+    assertEquals("unexpected status", 111, status);
+    assertBackendArgs(args, 2, (MockBackendHandler) ai.backendHandler);
+    assertCommandArgs(args, 1, (MockCommandHandler) ai.cmdHandlers.get(1));
+  }
+
+
+  @Test public void handleOne_Error()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "one", "xyz" };
+
+    try {
+      int status = ai.handle(args);
+      fail("exception not thrown, status="+status);
+    } catch (NumberFormatException expected) {
+      // expected
+    }
+  }
+
+
+  @Test public void handleTwo_OK()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "two", "111", "222" };
+
+    int status = ai.handle(args);
+
+    assertEquals("unexpected status", 222, status);
+    assertBackendArgs(args, 2, (MockBackendHandler) ai.backendHandler);
+    assertCommandArgs(args, 2, (MockCommandHandler) ai.cmdHandlers.get(2));
+  }
+
+
+  @Test public void handleOneTwo_OK()
+    throws Exception
+  {
+    ArgsInterpreter ai = createTestSubject();
+    String[] args = new String[]{ "server", "port", "one", "111", "112" };
+
+    int status = ai.handle(args);
+
+    assertEquals("unexpected status", 112, status);
+    assertBackendArgs(args, 2, (MockBackendHandler) ai.backendHandler);
+    assertCommandArgs(args, 2, (MockCommandHandler) ai.cmdHandlers.get(3));
+  }
+
 }
