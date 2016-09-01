2087e7e07a7a67493d627d612630f50c1b3d556d
Roland Weber <rolandweber@users.noreply.github.com>
2016-09-01 21:16:02 +0200

    parsing of message batches

--- src/main/java/pityoulish/sockets/client/TLVResponseParserImpl.java
+++ src/main/java/pityoulish/sockets/client/TLVResponseParserImpl.java
@@ -46,6 +46,10 @@ public class TLVResponseParserImpl implements ResponseParser
         visitor.visitError(text);
       } break;
 
+      case MESSAGE_BATCH: {
+        parseMessageBatch(tlv, visitor);
+      } break;
+
       case TICKET_GRANT: {
         String ticket =
           parseNestedStringValue(tlv, MsgBoardType.TICKET, "US-ASCII");
@@ -76,11 +80,14 @@ public class TLVResponseParserImpl implements ResponseParser
                                           String enc)
     throws Exception
   {
-    //@@@ check for end of data!
     MsgBoardTLV nested = parent.getNestedTLV();
-    if (nested.getType() != expect)
+    if (nested == null)
+       throw new Exception("@@@ missing nested type: "+expect); //@@@ NLS
+    else if (nested.getType() != expect)
        throw new Exception("@@@ unexpected nested type: "
                            +nested.getType()+"!="+expect); //@@@ NLS
+
+    //@@@ check for end of data!
 
     return parseStringValue(nested, enc);
   }
@@ -108,6 +115,116 @@ public class TLVResponseParserImpl implements ResponseParser
 
     return new String(tlv.getData(), tlv.getValueStart(),
                       tlv.getLength(), enc);
+  }
+
+
+  /**
+   * Parses a message batch and calls the visitor.
+   *
+   * @param mbtlv       the TLV with the message batch
+   * @param visitor     the visitor to invoke on the message batch elements
+   *
+   * @throws Exception in case of a problem
+   */
+  protected void parseMessageBatch(MsgBoardTLV mbtlv, Visitor visitor)
+    throws Exception
+  {
+    // The binary protocol specifies an order for the nested TLVs.
+    // The Marker and an optional Missed indicator appear before the messages.
+
+    MsgBoardTLV nested = mbtlv.getNestedTLV();
+    if (nested == null)
+       throw new Exception("@@@ missing nested type: MARKER"); //@@@ NLS
+    else if (nested.getType() != MsgBoardType.MARKER)
+       throw new Exception("@@@ unexpected nested type: "
+                           +nested.getType()+"!=MARKER"); //@@@ NLS
+    String marker = parseStringValue(nested, "US-ASCII");
+
+    boolean missed = false;
+    nested = nested.getNextTLV(mbtlv.getEnd());
+    if ((nested != null) && (nested.getType() == MsgBoardType.MISSED))
+     {
+       missed = true;
+       nested = nested.getNextTLV(mbtlv.getEnd());
+     }
+
+    visitor.enterMessageBatch(marker, missed);
+
+    while (nested != null)
+     {
+       if (nested.getType() != MsgBoardType.MESSAGE)
+          throw new Exception("@@@ unexpected nested type: "
+                              +nested.getType()+"!=MESSAGE"); //@@@ NLS
+       //@@@ check for end of data!
+
+       parseMessage(nested, visitor);
+       nested = nested.getNextTLV(mbtlv.getEnd());
+     }
+
+    visitor.leaveMessageBatch();
+  }
+
+
+  /**
+   * Parses a message and calls the visitor.
+   *
+   * @param msgtlv      the TLV with the message
+   * @param visitor     the visitor to invoke on the message
+   *
+   * @throws Exception in case of a problem
+   */
+  protected void parseMessage(MsgBoardTLV msgtlv, Visitor visitor)
+    throws Exception
+  {
+    //System.out.println("@@@ parsing MESSAGE at "+msgtlv.getStart());
+
+    // The order of elements within the message is not specified.
+    // Parse them as they come, then check if something is missing.
+    String originator = null;
+    String timestamp = null;
+    String text = null;
+
+    MsgBoardTLV nested = msgtlv.getNestedTLV();
+    while (nested != null)
+     {
+       //System.out.println("@@@ found "+nested.getType());
+
+       //@@@ check for end of data!
+       switch (nested.getType())
+        {
+         case ORIGINATOR:
+           if (originator != null)
+              throw new Exception("@@@ duplicate ORIGINATOR"); //@@@ NLS
+           originator = parseStringValue(nested, "US-ASCII");
+           break;
+
+         case TIMESTAMP:
+           if (timestamp != null)
+              throw new Exception("@@@ duplicate TIMESTAMP"); //@@@ NLS
+           timestamp = parseStringValue(nested, "US-ASCII");
+           break;
+
+         case TEXT:
+           if (text != null)
+              throw new Exception("@@@ duplicate TEXT"); //@@@ NLS
+           text = parseStringValue(nested, "UTF-8");
+           break;
+
+         default:
+           throw new Exception("@@@ unexpected nested type: "
+                               +nested.getType()); //@@@ NLS
+        }
+       nested = nested.getNextTLV(msgtlv.getEnd());
+     }
+
+    if (originator == null)
+       throw new Exception("@@@ missing ORIGINATOR"); //@@@ NLS
+    if (timestamp == null)
+       throw new Exception("@@@ missing TIMESTAMP"); //@@@ NLS
+    if (text == null)
+       throw new Exception("@@@ missing TEXT"); //@@@ NLS
+
+    visitor.visitMessage(originator, timestamp, text);
   }
 
 }
