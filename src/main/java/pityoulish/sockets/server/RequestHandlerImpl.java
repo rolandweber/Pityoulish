25c8ee6676a79f7be5fe0d84846b3c3be73ea564
Roland Weber <rolandweber@users.noreply.github.com>
2016-09-15 00:55:56 +0200

    Issue 20: more console output from the server

--- src/main/java/pityoulish/sockets/server/RequestHandlerImpl.java
+++ src/main/java/pityoulish/sockets/server/RequestHandlerImpl.java
@@ -73,7 +73,8 @@ public class RequestHandlerImpl implements RequestHandler
        response = dispatch(mbreq, address);
      } catch (Exception x) {
        response = rspBuilder.buildErrorResponse(x);
-       x.printStackTrace(System.out); //@@@ improve reporting?
+       describeException(x);
+       // x.printStackTrace(System.out); //@@@ trace to a logfile
      }
 
      return response;
@@ -94,31 +95,38 @@ public class RequestHandlerImpl implements RequestHandler
   protected ByteBuffer dispatch(MsgBoardRequest mbreq, InetAddress address)
     throws ProtocolException
    {
+     describeRequest(mbreq, address);
+
      ByteBuffer result = null;
      switch (mbreq.getReqType())
       {
        case LIST_MESSAGES: {
          MessageBatch mb = mbrHandler.listMessages(mbreq, address);
+         describeMessageBatch(mb);
          result = rspBuilder.buildMessageBatch(mb);
        } break;
 
        case PUT_MESSAGE: {
          String info = mbrHandler.putMessage(mbreq, address);
+         describeInfoResponse(info);
          result = rspBuilder.buildInfoResponse(info);
        } break;
 
        case OBTAIN_TICKET: {
          String tictok = mbrHandler.obtainTicket(mbreq, address);
+         describeTicketGrant(tictok);
          result = rspBuilder.buildTicketGrant(tictok);
        } break;
 
        case RETURN_TICKET: {
          String info = mbrHandler.returnTicket(mbreq, address);
+         describeInfoResponse(info);
          result = rspBuilder.buildInfoResponse(info);
        } break;
 
        case REPLACE_TICKET: {
          String tictok = mbrHandler.replaceTicket(mbreq, address);
+         describeTicketGrant(tictok);
          result = rspBuilder.buildTicketGrant(tictok);
        } break;
 
@@ -140,6 +148,88 @@ public class RequestHandlerImpl implements RequestHandler
   public ByteBuffer buildErrorResponse(Throwable cause)
   {
     return rspBuilder.buildErrorResponse(cause);
+  }
+
+
+  //@@@ move the descriptions to a dedicated helper?
+  //@@@ would improve symmetry with the client side
+
+  /**
+   * Describes an exception.
+   * The description is printed to <code>System.out</code>.
+   */
+  protected void describeException(Throwable axe)
+  {
+    if (axe instanceof ProtocolException)
+     {
+       // always contains a descriptive error message
+       System.out.println(axe.getLocalizedMessage()); // no trace
+     }
+    else
+     {
+       // something unexpected
+       System.out.println(axe.toString()); // no trace
+     }
+  }
+
+
+  /**
+   * Describes an incoming request.
+   * The description is printed to <code>System.out</code>.
+   *
+   * @param mbreq       the request to describe
+   * @param address     the network address of the client
+   */
+  protected void describeRequest(MsgBoardRequest mbreq, InetAddress address)
+  {
+    // For the classroom exercise, we don't need an exhaustive description
+    // of the request. Students should see that their request is coming in.
+    // And the messages put on the board should be visible to students and
+    // the instructor alike. For tickets, the returned ones are of interest.
+    // Just print all that get sent. Tickets conveniently include a username.
+    System.out.println(Catalog.DESCRIBE_REQUEST_TYPE_ADDR_2
+                       .format(mbreq.getReqType(), address));
+
+    if (mbreq.getText() != null)
+       System.out.println(Catalog.DESCRIBE_REQUEST_TICKET_TEXT_2
+                          .format(mbreq.getTicket(), mbreq.getText()));
+    else if (mbreq.getTicket() != null)
+       System.out.println(Catalog.DESCRIBE_REQUEST_TICKET_1
+                          .format(mbreq.getTicket()));
+  }
+
+
+  /**
+   * Describes a message batch.
+   * The description is printed to <code>System.out</code>.
+   */
+  protected void describeMessageBatch(MessageBatch mb)
+  {
+    System.out.println(Catalog.DESCRIBE_MESSAGE_BATCH_2
+                       .format(String.valueOf(mb.getMessages().size()),
+                               mb.getMarker()));
+    // Using String.valueOf on the number avoids locale-specific formatting.
+    // For example, it will print "1234" instead of "1,234" or "1.234".
+  }
+
+
+  /**
+   * Describes a granted ticket.
+   * The description is printed to <code>System.out</code>.
+   */
+  protected void describeTicketGrant(String tictok)
+  {
+    System.out.println(Catalog.DESCRIBE_TICKET_GRANT_1.format(tictok));
+  }
+
+
+  /**
+   * Describes an informational response.
+   * The description is printed to <code>System.out</code>.
+   */
+  protected void describeInfoResponse(String info)
+  {
+    // "OK" is implied
   }
 
 }
